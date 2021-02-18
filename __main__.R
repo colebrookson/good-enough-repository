3ce487dace93ddf4f8da0c29fe2c7104a3eddef2
Cole B. Brookson <brookson@ualberta.ca>
2021-02-18 10:39:10 -0700

    initialize __main__ file

---
+++ __main__.R
@@ -0,0 +1,10 @@
+########## 
+##########
+# This central R files successively sources all the R files needed to run a  
+# full set of analysis, and details the use of each 
+##########
+##########
+# AUTHOR: Cole B. Brookson
+# DATE OF CREATION: 2021-02-18
+##########
+##########
